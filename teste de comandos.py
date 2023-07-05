3add94f31346c2e21930a68b5848b9391851f2ec
Vital Fernando <135348523+vitalf03@users.noreply.github.com>
2023-07-05 15:36:25 -0300

    Add files via upload

---
+++ teste de comandos.py
@@ -0,0 +1,46 @@
+teste = []
+teste.append(1)
+
+print(teste)
+
+
+
+
+
+
+# tuple() é uma lista IMUTAVEL
+# list = [] é uma lista MUTAVEL
+# .strip = remove espaços em branco numa consulta
+#a função index é usada para encontrar o indice de um elemento em uma lista
+# len() retorno o comprimento(numero de elementos) de um objeto
+# .append() adiciona a uma LISTA em sua posição '0' 
+# .remove() remove um determinado elemento de uma lista
+# .extend() extende a lista adicionando todos os iteraveis
+# Se eu tenho um metodo que quero definir na minha classe mae, devo definir na class mae um abstractmethod utilizando : from abc import ABCMeta, abstractmethod
+#, ClassMãe(metaclass=ABCMETA), @abstractmethod(No metodo que quero definir para que todos possuam)
+# import numpy as np serve para importar arrays para uso numerico, funçoes numericas...
+# isinstance() verifica a instancia de um objeto 
+# __eq__(objeto) serve para atribuirmos igualdade entre dois objetos referenciados
+# utilizar o _ para passar dados que nao queremos
+# sorted() serve como função que ordena do menor pro maior
+#.sort() ordena uma lista do menor para o maior
+# Usamos o attrgetter para pegar um atributo atraves da key=attrgetter(nome_do_atributo). Lembrando que é necessario importar atraves do from operator import attrgetter
+#set() serve como funçao de conjunto que nao possui elementos repitidos
+# se a ordem nao importa podemos usar um conjunto atraves das {}
+# quando estamos trabalhando com conjutos utilizamos o | que serve como OU
+# quando estamos trabalhando com conjutos utilizamos o & que serve como E
+# quando estamos trabalhando com conjutos utilizamos o - que serve como interseção do primeiro conjunto ou seja(usuarios_data_science - usuarios_machine_learning) pega somente quem fez data science menos quem ja fez o machine learning
+# quando estamos trabalhando com conjutos utilizamos o ^ que serve para pegar o que tem diferente entre dois conjuntos
+# num conjunto utilizamos o .add para adicionar itens no conjunto
+# frozenset() congela o conjunto e nao permite realizar operacoes nele
+# del() deleta um item de um dicionario
+# values() pega os valores de um dicionario
+#items() pega os itens de um dicionario
+#lower() deixa a string em minusculo
+#defaultdict() deixa um valor padrao mas é necessario importar da seguinte maneira : from collections import defaultdict
+#Counter() conta o que tem dentro de um dicionario
+#split() quebra a linha com as palavras
+#sum() faz a conta do objeto referenciado
+
+
+
